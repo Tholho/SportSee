86e70c360e136116889ee980f330aa13bd1dfa5f
Nick Pseudo <tomdop.dakar@gmail.com>
2024-10-03 13:45:14 +0200

    Graphs layout adjustment

--- src/components/graphs.tsx
+++ src/components/graphs.tsx
@@ -10,7 +10,7 @@ const Graphs: React.FC<{ data: UserData }> = ({ data }) => {
     return (
         <div className="flex-col w-full">
             <ActivityG data={data.activity.data} />
-            <div className="flex w-full mt-2 h-1/2 gap-2">
+            <div className="flex w-full pt-2 h-1/2 gap-2">
                     <AverageSessions data={data.averageSessions.data} />
                     <PerformanceG data={data.performance.data} />
                     <ScoreG data={data.base.data} />                
