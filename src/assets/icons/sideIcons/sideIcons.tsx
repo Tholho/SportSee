9e110422541c5e8d1a45d97fa4c7b84600f317e8
Nick Pseudo <tomdop.dakar@gmail.com>
2024-10-11 16:06:06 +0200

    fix for icons loading on chrome

--- src/assets/icons/sideIcons/sideIcons.tsx
+++ src/assets/icons/sideIcons/sideIcons.tsx
@@ -1,12 +1,13 @@
 const importSideIcons = async (): Promise<{ [key: string]: string }> => {
     const context = import.meta.glob('./*.svg', { eager: true });
+    console.log(context);
     const icons: { [key: string]: string } = {};
   
     for (const path in context) {
       const key = path.replace('./', '').replace('.svg', '');
       icons[key] = (context[path] as any).default;
     }
-  
+    
     return icons;
   };
   
--- src/components/aside.tsx
+++ src/components/aside.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 import SideIcons from './sideIcons';
 import { sideIcons } from '../assets/icons/sideIcons/sideIcons';
 
-const sideIconsObj = await sideIcons;
 
 const Aside: React.FC = () => {
+  const [sideIconsObj, setSideIconsObj] = useState<{ [key: string]: string } | null>(null);
+
+  useEffect(() => {
+    const fetchIcons = async () => {
+      const icons = await sideIcons;
+      setSideIconsObj(icons);
+    };
+
+    fetchIcons();
+  }, []);
+
+  if (!sideIconsObj) {
+    return <div className="transform -rotate-90">Chargement</div>; // Gère un état de chargement si nécessaire
+  }
+
   return (
     <aside className=" bg-black text-white flex h-full absolute items-center ">
       <nav>
